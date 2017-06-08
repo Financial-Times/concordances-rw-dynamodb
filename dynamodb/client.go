ca73b38211573b1e35a5f9868399bc6b36b2cd05
Fernee, Julia <Julia.Fernee@ft.com>
2017-06-08 16:13:54 +0100

    Rewired tests to use locally deployed DynamoDB.

--- dynamodb/client.go
+++ dynamodb/client.go
@@ -4,14 +4,13 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 const (
-	TableHashKeyColumn = "conceptId"
-	TableDataColumn = "concordedIds"
+	TableHashKey = "conceptId"
 )
+
 type Model struct {
 	UUID         string   `json:"conceptId"`
 	ConcordedIds []string `json:"concordedIds"`
@@ -33,13 +32,11 @@ type DynamoDbClientImpl struct {
 func NewDynamoDbClient(dynamoDbTable string, awsRegion string) DynamoDbClient {
 	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String(awsRegion)}))
 	ddb := dynamodb.New(sess)
-	//fmt.Printf("%s", srv.Endpoint)
 	c := DynamoDbClientImpl{dynamoDbTable: dynamoDbTable, awsRegion: awsRegion, ddb: ddb}
 	return &c
 }
 
 func (s *DynamoDbClientImpl) Read(uuid string) (Model, error) {
-
 	input := &dynamodb.GetItemInput{}
 	input.SetTableName(s.dynamoDbTable)
 	k, err := dynamodbattribute.Marshal(uuid)
@@ -47,32 +44,29 @@ func (s *DynamoDbClientImpl) Read(uuid string) (Model, error) {
 	output, err := s.ddb.GetItem(input)
 	m := Model{}
 	err = dynamodbattribute.UnmarshalMap(output.Item, &m)
-	fmt.Printf(">>>>>>>> %v", m)
 	return m, err
 }
 
 func (s *DynamoDbClientImpl) Write(m Model) (model Model, err error) {
-
-	k, err := dynamodbattribute.Marshal(m.UUID)
-	l, err := dynamodbattribute.Marshal(m.ConcordedIds)
-	//fmt.Printf("\n 1: %v\n", key)
-	//fmt.Printf("\n 2: %v\n", L)
-
+	input, err := s.getUpdateInput(m)
+	model = Model{}
+	output, err := s.ddb.UpdateItem(input)
+	dynamodbattribute.ConvertFromMap(output.Attributes, &model)
+	return model, err
+}
+func (s *DynamoDbClientImpl) getUpdateInput(m Model) (*dynamodb.UpdateItemInput, error) {
 	input := &dynamodb.UpdateItemInput{}
-	input.SetKey(map[string]*dynamodb.AttributeValue{"conceptId": k})
+	k, err := dynamodbattribute.Marshal(m.UUID)
+	if err != nil { return input, err }
+	l, err := dynamodbattribute.Marshal(m.ConcordedIds)
+	if err != nil { return input, err }
+
+	input.SetKey(map[string]*dynamodb.AttributeValue{TableHashKey: k})
 	input.SetUpdateExpression("SET concordedIds = :concordedIds")
 	input.SetReturnValues(dynamodb.ReturnValueAllOld)
 	input.SetTableName(s.dynamoDbTable)
 	input.SetExpressionAttributeValues(map[string]*dynamodb.AttributeValue {":concordedIds" :l})
-	//fmt.Println(input.GoString())
-	//input.Validate()
-
-	model = Model{}
-	output, err := s.ddb.UpdateItem(input)
-	//fmt.Printf("\n len %v \n\n %s\n", len(output.GoString()), output.GoString())
-	dynamodbattribute.ConvertFromMap(output.Attributes, &model)
-	fmt.Printf(">>>>>>>> %v", model)
-	return model, err
+	return input, nil
 }
 
 func (s *DynamoDbClientImpl) Delete(uuid string) (model Model, err error) {
@@ -80,14 +74,12 @@ func (s *DynamoDbClientImpl) Delete(uuid string) (model Model, err error) {
 	input.SetReturnValues(dynamodb.ReturnValueAllOld)
 	input.SetTableName(s.dynamoDbTable)
 	k, err := dynamodbattribute.Marshal(uuid)
-	input.SetKey(map[string]*dynamodb.AttributeValue{"conceptId": k})
-	fmt.Println(input.GoString())
+	input.SetKey(map[string]*dynamodb.AttributeValue{TableHashKey: k})
 
 	output, err :=s.ddb.DeleteItem(input)
 
 	model = Model{}
 	dynamodbattribute.ConvertFromMap(output.Attributes, &model)
-	fmt.Printf(">>>>>>>> %v", model)
 	return model, err
 }
 
---
+++ dynamodb/client_test.go
@@ -0,0 +1,216 @@
+package dynamodbclient
+
+import (
+	"testing"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"fmt"
+)
+const (
+	UUID = "4f50b156-6c50-4693-b835-02f70d3f3bc0"
+	DDB_TABLE = "upp-concordance-store-semantic"
+	AWS_REGION = "eu-west-1"
+	DDB_ENDPOINT = "http://localhost:8000"
+)
+var goodModel = Model{
+	UUID: UUID,
+	ConcordedIds: []string{"7c4b3931-361f-4ea4-b694-75d1630d7746", "1e5c86f8-3f38-4b6b-97ce-f75489ac3113"},
+}
+
+var db *dynamodb.DynamoDB
+var c DynamoDbClientImpl
+
+var DescribeTableParams = &dynamodb.DescribeTableInput{	TableName: aws.String(DDB_TABLE)}
+
+func init() {
+	fmt.Println("Create DynamoDb \n")
+	db = setupDynamoDBLocal()
+	c = DynamoDbClientImpl{dynamoDbTable: DDB_TABLE, awsRegion: AWS_REGION, ddb: db}
+}
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("Create table \n")
+	createTableIfNotExists(t)
+
+	return func(t *testing.T) {
+		deleteTableIfExists(t)
+		t.Log("Destroy Table \n")
+	}
+}
+
+func setupDynamoDBLocal() *dynamodb.DynamoDB {
+	t := &testing.T{}
+	assert := assert.New(t)
+	sess, err := session.NewSession(&aws.Config{
+		Region:   aws.String(AWS_REGION),
+		Endpoint: aws.String(DDB_ENDPOINT)})
+	assert.NoError(err, "Should be able to create a session talking to local DynamoDB. Make sure this is running")
+	ddb := dynamodb.New(sess)
+	return ddb
+}
+
+func createTableIfNotExists(t *testing.T) error {
+	_, err := db.DescribeTable(DescribeTableParams)
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+
+			if awsErr.Code() == dynamodb.ErrCodeResourceNotFoundException {
+				params := &dynamodb.CreateTableInput{
+					AttributeDefinitions: []*dynamodb.AttributeDefinition{// Required
+						{// Required
+							AttributeName: aws.String(TableHashKey), // Required
+							AttributeType: aws.String(dynamodb.ScalarAttributeTypeS), // Required
+						},
+						// More values...
+					},
+					KeySchema: []*dynamodb.KeySchemaElement{// Required
+						{// Required
+							AttributeName: aws.String(TableHashKey), // Required
+							KeyType:       aws.String(dynamodb.KeyTypeHash), // Required
+						},
+						// More values...
+					},
+					ProvisionedThroughput: &dynamodb.ProvisionedThroughput{// Required
+						ReadCapacityUnits:  aws.Int64(5), // Required
+						WriteCapacityUnits: aws.Int64(5), // Required
+					},
+					TableName: aws.String(DDB_TABLE), // Required
+				}
+				_, err := db.CreateTable(params)
+				assert.NoError(t, err)
+			}
+		} else {
+			assert.Fail(t, fmt.Sprintf("Failed to connect to local DynamoDB. Error: %s", err.Error()))
+			return err
+		}
+
+	}
+	return err
+}
+
+func deleteTableIfExists(t* testing.T) error {
+	input := &dynamodb.DeleteTableInput{TableName: aws.String(DDB_TABLE)}
+	_, err := db.DeleteTable(input)
+	if  err == nil{
+		return nil
+	} else if err.(awserr.Error).Code() != dynamodb.ErrCodeResourceNotFoundException {
+		assert.Fail(t, "Failed to delete table. ", err.Error())
+	}else {
+		t.Log("Table doesn't exist")
+	}
+	return err
+}
+
+func TestDeleteTableThatExists(t *testing.T) {
+	createTableIfNotExists(t)
+	deleteTableIfExists(t)
+	_, err := db.DescribeTable(DescribeTableParams)
+	assert.True(t, (err.(awserr.Error).Code() == dynamodb.ErrCodeResourceNotFoundException))
+}
+
+func TestDeleteNonExistentTable(t *testing.T) {
+	deleteTableIfExists(t)
+	err := deleteTableIfExists(t)
+	assert.True(t, (err.(awserr.Error).Code() == dynamodb.ErrCodeResourceNotFoundException))
+}
+
+func TestCteateTableThatAlreadyExists(t *testing.T) {
+	createTableIfNotExists(t)
+	err := createTableIfNotExists(t)
+	assert.NoError(t, err)
+}
+func TestCreateTableIfNotExists(t *testing.T) {
+	deleteTableIfExists(t)
+	createTableIfNotExists(t)
+	_, err := db.DescribeTable(DescribeTableParams)
+	assert.NoError(t, err)
+}
+
+
+func TestUpdateInputIsValid(t *testing.T) {
+	input, err := c.getUpdateInput(goodModel)
+
+	assert.NoError(t, err, "Received error")
+	assert.NoError(t, input.Validate(), "Update Input is valid.")
+}
+
+func TestCreateConcordance(t *testing.T) {
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
+
+	oldModel, err := c.Write(goodModel)
+
+	assert.NoError(t, err, "Failed to write concordance.")
+	assert.Empty(t, oldModel.UUID)
+	newModel, err := c.Read(UUID);
+	assert.True(t, reflect.DeepEqual(goodModel, newModel), "Failed to create concordance record")
+}
+
+func TestUpdateConcordance(t *testing.T) {
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
+
+	_, err := c.Write(goodModel)
+	assert.NoError(t, err, "Failed to write concordance.")
+	newModel := Model{
+		UUID: "4f50b156-6c50-4693-b835-02f70d3f3bc0",
+		ConcordedIds: []string{"7c4b3931-361f-4ea4-b694-75d1630d7746"},
+	}
+	oldModel, err := c.Write(newModel)
+
+	updatedModel, err := c.Read(UUID);
+
+	assert.True(t, reflect.DeepEqual(goodModel, oldModel), "Failed to retrive old concordance record")
+	assert.True(t, reflect.DeepEqual(newModel, updatedModel), "Failed to update concordance record")
+}
+
+func TestDeleteExistingConcordance(t *testing.T) {
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
+
+	_, err := c.Write(goodModel)
+	assert.NoError(t, err, "Failed to set up concordance to be deleted")
+
+	oldModel, err := c.Delete(UUID)
+
+	assert.NoError(t, err, "Deletion operation resulted in error.")
+	assert.True(t, reflect.DeepEqual(goodModel, oldModel), "Failed to retrive old concordance record upon deletion")
+}
+
+func TestDeleteNonExistingConcordance(t *testing.T) {
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
+
+	oldModel, err := c.Delete(UUID)
+
+	assert.NoError(t, err, "Deletion operation resulted in error.")
+	assert.Empty(t, oldModel.UUID, "Failed to retrive old concordance record upon deletion")
+}
+
+func TestReadExistingConcordance(t *testing.T) {
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
+
+	_, err := c.Write(goodModel)
+	assert.NoError(t, err, "failed to set up concordance to be read.")
+
+	model, err := c.Read(UUID)
+
+	assert.NoError(t, err, "Retrieving concordance resulted in error.")
+	assert.True(t, reflect.DeepEqual(goodModel, model), "Failed to retrive old concordance record")
+}
+
+func TestReadNonExistingConcordance(t *testing.T) {
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
+
+	model, err := c.Read(UUID)
+
+	assert.NoError(t, err, "Retrieving concordance resulted in error.")
+	assert.Empty(t, model.UUID, "Failed to retrive old concordance record upon deletion")
+	assert.Empty(t, model.ConcordedIds, "Failed to retrive old concordance record upon deletion")
+}
+
